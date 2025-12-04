5d473be96d73b0470527e2f69a5aa4c015daa57a
nschrag <nschrag@gmail.com>
2025-12-04 16:37:55 -0600

    Fixed bugs with candles being extinguished multiple times
    - Timers were firing repeatedly
    - New timers were being started by already extinguished candles

--- candle.gd
+++ candle.gd
@@ -3,17 +3,21 @@ class_name Candle extends Node3D
 signal flame_extinguished
 
 @export var flame: Node3D
+@export var collider: Area3D
 var resilience: float = 0.5
 
 var timer: Timer
 
 func extinguish():
+	assert(flame.visible)
+	collider.monitoring = false
 	flame.visible = false
 	flame_extinguished.emit()
 
 func _on_mouse_entered_flame() -> void:
 	if timer == null:
 		timer = Timer.new()
+		timer.one_shot = true
 		timer.timeout.connect(extinguish)
 		add_child(timer)
 	
